1f2a443258fc35a8799434bcef23b29e9af5de44
Nabilbmk9 <nabil.bmk9@gmail.com>
2024-04-26 12:49:48 +0200

    Ajout requirements.txt

--- src/chatgpt_manager.py
+++ src/chatgpt_manager.py
@@ -1,7 +1,12 @@
+from config import get_env_variable
+
+
 class ChatGPTManager:
     def __init__(self, api_key):
-        self.api_key = api_key
+        self.api_key = get_env_variable('GPT_KEY')
 
     def send_message(self, message):
         # Intégration avec l'API de ChatGPT pour envoyer un message
         pass
+
+
