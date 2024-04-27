a24da2a4d02cf11cf1dffb2d3f3e8bc7d34c3809
Nabilbmk9 <nabil.bmk9@gmail.com>
2024-04-27 10:59:36 +0200

    ajout clé api openai

--- src/chatgpt_manager.py
+++ src/chatgpt_manager.py
@@ -1,9 +1,11 @@
 from config import get_env_variable
+import openai
 
 
 class ChatGPTManager:
     def __init__(self, api_key):
         self.api_key = get_env_variable('GPT_KEY')
+        self.client = openai.OpenAI(api_key=api_key)
 
     def send_message(self, message):
         # Intégration avec l'API de ChatGPT pour envoyer un message
--- src/main_controller.py
+++ src/main_controller.py
@@ -8,11 +8,12 @@ from config import get_env_variable
 class MainController:
     def __init__(self):
         self.browser_manager = BrowserManager(headless=False, block_images=False)
-        self.scraper = LinkedInScraper(self.browser_manager.new_page())
-        self.chat_manager = ChatGPTManager(api_key='your_chatgpt_api_key')
-        self.data_manager = DataManager(db_connection='your_database_connection_string')
         self.username = get_env_variable('LINKEDIN_USERNAME')
         self.password = get_env_variable('LINKEDIN_PASSWORD')
+        self.api_key = get_env_variable('GPT_API_KEY')
+        self.scraper = LinkedInScraper(self.browser_manager.new_page())
+        self.chat_manager = ChatGPTManager(api_key=self.api_key)
+        self.data_manager = DataManager(db_connection='your_database_connection_string')
 
     def run(self):
         self.scraper.login(self.username, self.password)
