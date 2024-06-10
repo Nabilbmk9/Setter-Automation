3d933275b916bafa62aceaa260a7d316f0492124
Nabilbmk9 <nabil.bmk9@gmail.com>
2024-06-10 10:05:34 +0200

    docstring

--- src/main_controller.py
+++ src/main_controller.py
@@ -8,7 +8,7 @@ from config import get_env_variable
 class MainController:
     def __init__(self):
         self.browser_manager = BrowserManager(headless=False, block_images=False)
-        self.username = get_env_variable('LINKEDIN_USERNAME')
+        self.username = get_env_variable('LINKEDIN_EMAIL')
         self.password = get_env_variable('LINKEDIN_PASSWORD')
         self.api_key = get_env_variable('GPT_API_KEY')
         self.scraper = LinkedInScraper(self.browser_manager.new_page())
@@ -20,6 +20,8 @@ class MainController:
         self.scraper.suspect_verification()
         self.scraper.fetch_unread_messages()
         self.browser_manager.close()
+
+
 
 
 if __name__ == '__main__':
