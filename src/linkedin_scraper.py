5b1beb11572fcbdbde5663e37b23e0dd6c50ac97
Nabilbmk9 <nabil.bmk9@gmail.com>
2024-06-11 05:53:30 +0200

    Boucle complet pour envoi de message + log

--- src/linkedin_scraper.py
+++ src/linkedin_scraper.py
@@ -1,39 +1,74 @@
+import logging
 from src.utils import remove_emojis
 
+# Configurer le logger
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 class LinkedInScraper:
     def __init__(self, page):
         self.page = page
 
+    # Méthodes publiques
     def login(self, username, password):
         self.page.goto("https://www.linkedin.com/login")
         self.page.get_by_label("E-mail ou téléphone").fill(username)
         self.page.get_by_label("Mot de passe").fill(password)
         self.page.get_by_label("S’identifier", exact=True).click()
-
-    def check_for_email_verification_pin(self):
-        try:
-            self.page.wait_for_selector('#input__email_verification_pin', timeout=5000)
-            print("Page d'authentification détectée avec ID. En attente du code de vérification.")
-            return True
-        except:
-            return False
+        logger.info("Login effectué")
 
     def ensure_authenticated(self):
-        while self.check_for_email_verification_pin():
+        while self._check_for_email_verification_pin():
             self.page.wait_for_timeout(10000)
-
-    def fetch_unread_messages(self):
-        self.page.get_by_role("link", name="Messagerie").click()
-        self.page.get_by_role("radio", name="Non lus").click()
-        self._load_all_messages()
+        logger.info("Authentification vérifiée")
 
     def get_all_profiles_on_page(self):
         self.page.wait_for_selector('li.reusable-search__result-container')
         all_profiles_list = self._fetch_profiles_list()
         all_profiles_info = [self._extract_profile_info(profile_content) for profile_content in all_profiles_list]
-        print(all_profiles_info)
+        logger.info(f"{len(all_profiles_info)} profils trouvés sur la page")
         return all_profiles_info
+
+    def click_connect_or_more_button(self):
+        """Gère le clic sur 'Se connecter' ou 'Plus' pour accéder à 'Se connecter'."""
+        action_buttons, button_texts = self._get_profile_action_buttons()
+
+        # Essayer de cliquer sur "Se connecter"
+        if self._click_button_by_text(action_buttons, "Se connecter"):
+            logger.info("Bouton 'Se connecter' cliqué")
+            self._click_add_note_button()
+            return
+
+        # Sinon, cliquer sur "Plus" puis "Se connecter" dans le menu déroulant
+        if self._click_button_by_text(action_buttons, "Plus"):
+            logger.info("Bouton 'Plus' cliqué")
+            if self._click_connect_button_from_dropdown():
+                logger.info("Bouton 'Se connecter' cliqué dans le menu déroulant")
+                self._click_add_note_button()
+
+    def enter_custom_message(self, first_name, message_template):
+        """Remplace {first_name} dans le message et l'écrit dans le textarea."""
+        custom_message = message_template.replace("{first_name}", first_name)
+        try:
+            self.page.wait_for_selector('textarea[name="message"]', timeout=5000)  # Attendre que le textarea soit visible
+            message_textarea = self.page.query_selector('textarea[name="message"]')
+            if message_textarea:
+                message_textarea.fill(custom_message)
+                logger.info(f"Message personnalisé écrit pour {first_name}")
+                self._click_send_invitation_button()
+            else:
+                logger.error("Textarea non trouvé")
+        except Exception as e:
+            logger.error(f"Erreur lors de la sélection du textarea : {e}")
+
+    # Méthodes privées
+    def _check_for_email_verification_pin(self):
+        try:
+            self.page.wait_for_selector('#input__email_verification_pin', timeout=5000)
+            logger.info("Page d'authentification détectée avec ID. En attente du code de vérification.")
+            return True
+        except:
+            return False
 
     def _fetch_profiles_list(self):
         return self.page.query_selector_all('li.reusable-search__result-container')
@@ -55,12 +90,58 @@ class LinkedInScraper:
         last_name = name_parts[1] if len(name_parts) > 1 else ""
 
         return {
-            "full_name": full_name,
+            "full_name": f"{first_name} {last_name}",
             "first_name": first_name,
             "last_name": last_name,
             "linkedin_profile_link": linkedin_profile_link,
             "connect_or_follow": connect_or_follow
         }
+
+    def _get_profile_action_buttons(self):
+        """Récupère les boutons d'action de profil et leurs textes."""
+        action_buttons = self.page.query_selector_all('.pvs-profile-actions__action')
+
+        button_texts = [button.inner_text().strip() for button in action_buttons]
+
+        return action_buttons, button_texts
+
+    def _click_button_by_text(self, action_buttons, button_text):
+        """Clique sur le bouton avec le texte spécifié."""
+        for button in action_buttons:
+            text = button.inner_text().strip()
+            if text == button_text:
+                button.click()
+                return True
+        return False
+
+    def _click_connect_button_from_dropdown(self):
+        """Clique sur le bouton 'Se connecter' dans le menu déroulant."""
+        self.page.wait_for_timeout(1000)  # Attendre que le menu déroulant apparaisse
+        dropdown_buttons = self.page.query_selector_all('.artdeco-dropdown__item--is-dropdown')
+        for dropdown_button in dropdown_buttons:
+            dropdown_text = dropdown_button.inner_text().strip()
+            if dropdown_text == "Se connecter":
+                dropdown_button.click()
+                return True
+        return False
+
+    def _click_add_note_button(self):
+        """Clique sur le bouton 'Ajouter une note' après avoir cliqué sur 'Se connecter'."""
+        self.page.wait_for_timeout(1000)  # Attendre que le bouton apparaisse
+        add_note_button = self.page.query_selector('button[aria-label="Ajouter une note"]')
+        if add_note_button:
+            add_note_button.click()
+
+    def _click_send_invitation_button(self):
+        """Clique sur le bouton 'Envoyer' après avoir écrit le message."""
+        self.page.wait_for_timeout(1000)  # Attendre que le bouton apparaisse
+        send_button = self.page.query_selector('button[aria-label="Envoyer une invitation"]')
+        if send_button:
+            send_button.click()
+            logger.info("Invitation envoyée")
+        else:
+            logger.error("Bouton 'Envoyer une invitation' non trouvé")
+
 
     def _load_all_messages(self):
         # Attente explicite pour que la page se charge initialement
--- src/main_controller.py
+++ src/main_controller.py
@@ -1,6 +1,6 @@
 from browser_manager import BrowserManager
 from linkedin_scraper import LinkedInScraper
-from chatgpt_manager import ChatGPTManager
+# from chatgpt_manager import ChatGPTManager
 from data_manager import DataManager
 from config import get_env_variable
 
@@ -11,20 +11,26 @@ class MainController:
         self.username = get_env_variable('LINKEDIN_EMAIL')
         self.password = get_env_variable('LINKEDIN_PASSWORD')
         self.search_link = get_env_variable('LINKEDIN_SEARCH_LINK')
+        self.message_template = get_env_variable('MESSAGE')
         self.api_key = get_env_variable('GPT_API_KEY')
         self.scraper = LinkedInScraper(self.browser_manager.new_page())
-        self.chat_manager = ChatGPTManager(api_key=self.api_key)
+        # self.chat_manager = ChatGPTManager(api_key=self.api_key)
         self.data_manager = DataManager(db_connection='your_database_connection_string')
 
     def run(self):
         self.scraper.login(self.username, self.password)
-        self.scraper.suspect_verification()
+        self.scraper.ensure_authenticated()
         self.scraper.page.goto(self.search_link)
-        self.scraper.get_all_profiles_on_page()
+
+        profiles = self.scraper.get_all_profiles_on_page()
+        for profile in profiles:
+            if profile:
+                self.scraper.page.goto(profile.get('linkedin_profile_link'))
+                self.scraper.click_connect_or_more_button()
+                self.scraper.enter_custom_message(profile.get('first_name'), self.message_template)
+
         # self.scraper.fetch_unread_messages()
-        self.browser_manager.close()
-
-
+                self.browser_manager.close()
 
 
 if __name__ == '__main__':
