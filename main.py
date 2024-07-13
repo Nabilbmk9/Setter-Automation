0d7694d3ff92061512ff2ef5f8042a06ba48e73c
Nabilbmk9 <nabil.bmk9@gmail.com>
2024-07-13 21:35:00 +0200

    Gestion des logs en mode debug ou non

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from ui.main_window import MainWindow
 from ui.announcement_window import AnnouncementWindow
 from ui.styles import get_stylesheet
 from utils.requests_handler import fetch_announcement, check_for_updates
+from config.logging_config import setup_logging
 
 
 def get_resource_path(relative_path):
@@ -40,36 +41,19 @@ def load_config(file_path):
         return json.load(f)
 
 
-def get_log_file_path():
-    appdata_dir = os.getenv('APPDATA')
-    log_file = os.path.join(appdata_dir, 'linkedin_automation_bot', 'app.log')
-    os.makedirs(os.path.dirname(log_file), exist_ok=True)
-    return log_file
-
-
-def setup_logging():
-    log_file = get_log_file_path()
-    logging.basicConfig(
-        filename=log_file,
-        level=logging.DEBUG,
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    )
-
-
 def main():
-    # Configurer la journalisation pour écrire dans un fichier
-    setup_logging()
-    logging.debug("Application started")
+    # Configurer la journalisation pour écrire dans un fichier ou dans le terminal
+    logger = setup_logging()
+    logger.debug("Application started")
 
-    # Log the current working directory for debugging
-    logging.debug(f"Current working directory: {os.getcwd()}")
+    logger.debug(f"Current working directory: {os.getcwd()}")
 
     # Charger les configurations
     try:
         user_config = load_config('config/user_config.json')
         app_config = load_config('config/app_config.json')
     except Exception as e:
-        logging.error(f"Error loading configuration: {e}")
+        logger.error(f"Error loading configuration: {e}")
         return
 
     current_version = app_config['current_version']
@@ -79,12 +63,12 @@ def main():
 
     # Test de l'importation de sqlite3
     try:
-        logging.debug("Testing sqlite3 import and connection...")
+        logger.debug("Testing sqlite3 import and connection...")
         conn = sqlite3.connect(':memory:')
-        logging.debug("Successfully connected to sqlite3 in-memory database.")
+        logger.debug("Successfully connected to sqlite3 in-memory database.")
         conn.close()
     except Exception as e:
-        logging.error(f"Error during sqlite3 test: {e}")
+        logger.error(f"Error during sqlite3 test: {e}")
         raise
 
     try:
@@ -92,15 +76,15 @@ def main():
 
         # Appliquer le style globalement
         app.setStyleSheet(get_stylesheet())
-        logging.debug("Stylesheet applied")
+        logger.debug("Stylesheet applied")
 
         # Vérifier les mises à jour
         has_update, latest_version = check_for_updates(current_version, version_url)
-        logging.debug(f"Update check completed: has_update={has_update}, latest_version={latest_version}")
+        logger.debug(f"Update check completed: has_update={has_update}, latest_version={latest_version}")
 
         # Récupérer l'annonce dynamique
         announcement_message = fetch_announcement(announcement_url)
-        logging.debug(f"Fetched announcement: {announcement_message}")
+        logger.debug(f"Fetched announcement: {announcement_message}")
 
         # Construire le message combiné
         combined_message = ""
@@ -111,27 +95,27 @@ def main():
         elif has_update:
             combined_message = f"Une nouvelle version ({latest_version}) est disponible !"
 
-        logging.debug(f"Combined message: {combined_message}")
+        logger.debug(f"Combined message: {combined_message}")
 
         # Créer la fenêtre principale
         main_window = MainWindow()
-        logging.debug("Main window created")
+        logger.debug("Main window created")
 
         # Afficher la fenêtre d'annonces si nécessaire
         if combined_message:
             announcement_window = AnnouncementWindow(combined_message, update_url if has_update else None,
                                                      parent=main_window)
-            logging.debug("Announcement window created")
+            logger.debug("Announcement window created")
             announcement_window.exec()
-            logging.debug("Announcement window executed")
+            logger.debug("Announcement window executed")
 
         # Afficher la fenêtre principale
         main_window.show()
-        logging.debug("Main window shown")
+        logger.debug("Main window shown")
 
         sys.exit(app.exec())
     except Exception as e:
-        logging.error(f"An error occurred: {e}")
+        logger.error(f"An error occurred: {e}")
         raise
 
 
